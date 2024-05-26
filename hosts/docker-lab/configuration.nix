3998d56d8975e87c1627f0ac39f2ba8ec277d0c9
Joshua Lee <josh@joshuamlee.com>
2024-05-26 11:07:20 -0400

    docker lab

--- hosts/docker-lab/configuration.nix
+++ hosts/docker-lab/configuration.nix
@@ -4,18 +4,12 @@
   imports = [
     ../vm/configuration.nix
     ../vm/hardware-configuration.nix
-    ../common/nodhcp.nix
-    ../common/swap.nix
     ../../modules/node-exporter.nix
-    # ../../modules/portainer.nix
+    ../../modules/portainer.nix
     ../../users/kasti.nix
   ];
 
-  networking.hostName = "okvir";
-  networking.interfaces.eth0.ipv4.addresses = [ {
-    address = "192.168.0.102";
-    prefixLength = 24;
-  } ];
+  networking.hostName = "docker-lab";
 
   networking.firewall = {
     enable = false;
