9861205ebe76a0a3f73f537831fcc87d4814d318
Joshua Lee <josh@joshuamlee.com>
2024-05-27 19:53:43 -0400

    always expose node exporter port

--- modules/node-exporter.nix
+++ modules/node-exporter.nix
@@ -1,12 +1,15 @@
 { config, pkgs, ... }:
-
+let
+  prometheus_exporter_port = 9002;
+in
 {
+  networking.firewall.allowedTCPPorts = [ prometheus_exporter_port ];
   services.prometheus = {
     exporters = {
       node = {
         enable = true;
         enabledCollectors = [ "systemd" ];
-        port = 9002;
+        port = prometheus_exporter_port;
       };
     };
   };
