1973e12dad64a5c6b03c6adcfa9089995d395376
Josh Lee <josh@joshuamlee.com>
2024-10-08 21:19:53 -0400

    add alias for resize tmux status bar

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -17,12 +17,15 @@
       ta = "tmux attach";
       n = "nvim .";
       k = "kubectl";
+      tmux-left = "tmux set-option status-left-length 40";
     };
     initExtra = ''
       eval "$(zoxide init zsh)"
 
       # aws cli auto complete
       complete -C '${pkgs.awscli}/bin/aws_completer' aws
+
+      DISABLE_AUTO_TITLE="true"
     '';
   };
 }
